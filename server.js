170ed7bdd7911e71d876432fdd5a721e796d00f4
Alexander Kotov <alexanderko@interlink-ua.com>
2018-07-18 17:51:27 +0300

    split modules into routes and app

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require('express')
-const app = express()
-
-app.use(express.json())
-
-app.post('/:say', function (req, res) {
-  let say = req.params.say
-  let { first, last } = req.body
-  let message = `${say} ${first} ${last}!`
-  res.json({ message });
-})
+const app = require('./app')
 
 app.listen(3000, function () {
   console.log('Server started on port 3000');
